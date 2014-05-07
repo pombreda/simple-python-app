52bba31a2971d0916b737078dd9cf446af484f72
Beyang Liu <beyang.liu@gmail.com>
2014-05-07 15:19:48 -0700

    basic todo

---
+++ app.py
@@ -0,0 +1,21 @@
+import string
+import json
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+bulletin_board = []
+
+@app.route("/")
+def get_list():
+    html = string.join([('<p>%s</p>' % item) for item in bulletin_board], '\n')
+    return make_response(html)
+
+@app.route("/item", methods=['PUT'])
+def add_list():
+    name = request.args.get('name')
+    bulletin_board.append(name)
+    return make_response(json.dumps({'status': 'success'}))
+
+if __name__ == "__main__":
+    app.run(debug=True)
