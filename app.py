67ad2aa113acb3a57d6e989155b4efc183e11d72
Beyang Liu <beyang.liu@gmail.com>
2014-05-07 16:09:30 -0700

    todo -> bbaas

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from jinja2 import Template
 
 DB_CONFIG = {
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:////tmp/todo-list.db',
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:////tmp/bbaas.db',
     'SQLALCHEMY_ECHO': False,
     'SECRET_KEY': '',
     'DEBUG': True,
@@ -16,24 +16,24 @@ app = Flask(__name__)
 app.config.update(DB_CONFIG)
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class BBItem(db.Model):
     def __init__(self, name):
         self.name = name
         self.time = datetime.utcnow()
 
-    __tablename__ = 'todos'
+    __tablename__ = 'board'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String())
     pub_date = db.Column(db.DateTime)
 
 @app.route("/")
 def get_list():
-    return render_template('list.html', items=Todo.query.order_by(Todo.pub_date.desc()).all())
+    return render_template('list.html', items=BBItem.query.order_by(BBItem.pub_date.desc()).all())
 
 @app.route("/item", methods=['PUT'])
 def add_list():
     name = request.args.get('name')
-    new_item = Todo(name)
+    new_item = BBItem(name)
     db.session.add(new_item)
     db.session.commit()
     return make_response(json.dumps({'status': 'success'}))
