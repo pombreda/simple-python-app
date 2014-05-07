b529d89a86e74a4e7ea65da22d12c0c6605c39f5
Beyang Liu <beyang.liu@gmail.com>
2014-05-07 15:19:48 -0700

    stuff todo

---
+++ app.py
@@ -0,0 +1,5 @@
+# TODO: keep track of bulletin board state
+
+# TODO: route "/" -> returns the bulletin board HTML
+
+# TODO: route PUT "/item" -> adds item to bulletin board, returns some JSON indicating success
