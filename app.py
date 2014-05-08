5ba894ce9cd0973adb4d1c95662d0b2ca9ca99e1
Beyang Liu <beyang.liu@gmail.com>
2014-05-07 17:12:30 -0700

    hello flask

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-# TODO: keep track of bulletin board state
+from flask import *
 
-# TODO: route "/" -> returns the bulletin board HTML
+app = Flask(__name__)
 
-# TODO: route PUT "/item" -> adds item to bulletin board, returns some JSON indicating success
+# keep track of bulletin board state
+bulletin_board = []
+
+# route "/" -> returns the bulletin board HTML
+@app.route('/')
+def bb():
+    # TODO
+    return 'hello world'
+
+# TODO route PUT "/item" -> adds item to bulletin board, returns some JSON indicating success
+
+if __name__ == '__main__':
+    app.run(debug=True)
